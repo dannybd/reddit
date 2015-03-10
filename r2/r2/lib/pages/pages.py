6584284d1ea93f42446246dc1aa89aa9f8d9330b
Discussion @ MIT team <discussion-web@mit.edu>
2015-03-10 03:30:35 -0400

    Adding a dummy /login_shib page for routing back traffic

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -911,6 +911,7 @@ class RedditFooter(CachedTemplate):
                 type = "flat_vert",
                 separator = "")
         ]
+        self.nav = []
         CachedTemplate.__init__(self)
 
 class ClickGadget(Templated):
